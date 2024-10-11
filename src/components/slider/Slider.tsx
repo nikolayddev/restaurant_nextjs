2e55098ce2824bf3c6efa54590cf234b0d5e0e34
nikolayddev <122819108+nikolayddev@users.noreply.github.com>
2024-10-11 16:02:33 +0300

    Created Featured

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -24,23 +24,25 @@ const data = [
 export default function Slider() {
     const [currentSlide, setCurrentSlide] = useState(0);
 
-    // useEffect(() => {
-    //     const interval = setInterval(
-    //         () => setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)), 4000
-    //     );
+    useEffect(() => {
+        const interval = setInterval(
+            () => setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)), 4000
+        );
 
-    //     return () => clearInterval(interval);
-    // }, []);
+        return () => clearInterval(interval);
+    }, []);
 
 
     return (
         <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
+            {/* TEXT CONTAINER  */}
             <div className="flex-1 flex flex-col text-red-500 font-bold justify-center items-center gap-8">
                 <h1 className="text-5xl uppercase p-4 md:p-4 text-center md:text-6xl xl:text-7xl">
                     {data[currentSlide].title}
                 </h1>
                 <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
             </div>
+            {/* IMAGE CONTAINER */}
             <div className="w-full flex-1 relative">
                 <Image src={data[currentSlide].image} alt="" fill className="object-cover" />
             </div>
