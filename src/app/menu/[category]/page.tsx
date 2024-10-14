0451d5bd69e908ce5a1f8912e180a009e862b7e0
nikolayddev <122819108+nikolayddev@users.noreply.github.com>
2024-10-14 17:23:00 +0300

    Created Menu and Categories pages

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,5 +1,27 @@
+import { pizzas } from "@/data";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function CategoryPage() {
     return (
-        <div>Category page</div>
+        <div className="flex flex-wrap text-red-500">
+            {pizzas.map(item => (
+                <Link className="w-full h-[60vh] border-r-2 border-b-2 sm:w-1/2 lg:w-1/3 border-red-500 p-4 flex flex-col justify-between group even:bg-fuchsia-50"
+                    href={`/product/${item.id}`}
+                    key={item.id}
+                >
+                    {/* IMAGE CONTAINER */}
+                    <div className="relative h-[80%]">
+                        {item.img && <Image src={item.img} alt="" fill className="object-contain"/>}
+                    </div>
+                    {/* TEXT CONTAINER */}
+                    <div className="flex items-center justify-between font-bold">
+                        <h1 className="text-2xl uppercase p-2">{item.title}</h1>
+                        <h2 className="group-hover:hidden text-xl">${item.price}</h2>
+                        <button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">Add to Cart</button>
+                    </div>
+                </Link>
+            ))}
+        </div>
     );
 }
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,19 @@
+import { singleProduct } from "@/data";
+import Image from "next/image";
+
 export default function SingleProductPage() {
     return (
-        <div>SingleProduct</div>
+        <div className="p-4 lg-px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row ">
+            {/* IMAGE CONTINER */}
+            {singleProduct.img && <div className="relative h-[70vh]">
+                <Image src={singleProduct.img} alt="" fill className="object-fit" />
+            </div>
+            }
+            {/* TEXT CONTAINER */}
+            <div className="">
+                <h1>{singleProduct.title}</h1>
+                <p>{singleProduct.desc}</p>
+            </div>
+        </div>
     );
 }
